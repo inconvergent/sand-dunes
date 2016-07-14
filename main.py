efc1c159e247e0950a408554225f2b8a7c2c809b
Anders Hoff <inconvergent@gmail.com>
2016-07-14 22:47:31 +0200

    shadow map.

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
+from numpy import zeros
+
 
 SIZE = 512
 IMG = './img/x512-text.png'
-GRAINS = 500
 ONE = 1./SIZE
 
 LEAP = 10000
@@ -12,63 +13,73 @@ LEAP = 10000
 BACK = [1,1,1,1]
 FRONT = [0,0,0,5]
 
-ANGLE_STP = 0.001
 
 def get_initial(img):
   from numpy.random import random
-  from scipy.ndimage.filters import gaussian_filter
   from modules.helpers import get_img_as_rgb_array
 
-  initial = get_img_as_rgb_array(img)[:,:,0].squeeze()
-  initial *= 200
-  initial += 50
-  initial += random(initial.shape)*10.0
+  # initial = zeros((SIZE,SIZE), 'float')
+  # initial[0,0] = 1
+  # initial[1,0] = 1
+  # initial[1,3] = 1
+  # initial[2,0] = 2
+  # initial[2,1] = 2
+  # initial[10,3:] = 1
 
-  gaussian_filter(
-    initial,
-    2,
-    output=initial,
-    order=0,
-    mode='mirror'
-    )
+  initial = get_img_as_rgb_array(img)[:,:,0].squeeze()
+  initial *= 20
+  initial += random(initial.shape)*5.0
+
+  # from scipy.ndimage.filters import gaussian_filter
+  # gaussian_filter(
+  #   initial,
+  #   2,
+  #   output=initial,
+  #   order=0,
+  #   mode='mirror'
+  #   )
 
   return initial.astype('int')
 
 
 def main():
 
+  from numpy import dstack
   from modules.dunes import Dunes
   from sand import Sand
   from fn import Fn
 
-  # from numpy import dstack
-
   initial = get_initial(IMG)
-  dunes = Dunes(
-      SIZE,
-      initial,
-      grains=GRAINS,
-      angle_stp=ANGLE_STP
-      )
+  print(initial)
+  print()
+
+  dunes = Dunes(initial)
 
   sand = Sand(SIZE)
   sand.set_rgba(FRONT)
   fn = Fn(prefix='./res/', postfix='.png')
 
-  try:
-    while True:
-      dunes.step()
+  bw = dunes.get_normalized_sand()
+  shadow = dunes.get_shadow()
+  rgb = dstack((zeros(bw.shape,'float'),bw,1.0-shadow))
+  sand.set_bg_from_rgb_array(rgb)
 
-      if dunes.i % LEAP == 0:
-        print(dunes.i)
-        bw = dunes.get_normalized_sand()
-        # rgb = dstack((bw,bw,bw))
-        sand.set_bg_from_bw_array(bw)
-        name = fn.name()
-        sand.write_to_png(name)
+  sand.write_to_png('test.png')
 
-  except KeyboardInterrupt:
-    pass
+  # try:
+  #   while True:
+  #     dunes.step()
+  #
+  #     if dunes.i % LEAP == 0:
+  #       print(dunes.i)
+  #       bw = dunes.get_normalized_sand()
+  #       # rgb = dstack((bw,bw,bw))
+  #       sand.set_bg_from_bw_array(bw)
+  #       name = fn.name()
+  #       sand.write_to_png(name)
+  #
+  # except KeyboardInterrupt:
+  #   pass
 
 
 if __name__ == '__main__':
--- modules/dunes.py
+++ modules/dunes.py
@@ -6,6 +6,7 @@ from numpy import cos
 from numpy import sin
 from numpy import array
 from numpy import zeros
+from numpy import ones
 
 from numpy import logical_not
 from numpy import logical_or
@@ -20,30 +21,53 @@ class Dunes(object):
 
   def __init__(
       self,
-      size,
-      initial,
-      grains=10,
-      angle_stp=0.01
+      initial
       ):
+    size, tmp = initial.shape
+    if not size==tmp:
+      raise ValueError('initial must be square')
+
     self.size = size
-    self.size2 = size*size
     self.one = 1.0/size
-    self.angle_stp = angle_stp
-    self.grains = grains
+    self.delta = 4
+    self.i = 0
 
     self.stp = self.one
     self.sand = zeros((size,size), 'int')
     self.sand[:,:] = initial
-    self.xy = random(size=(grains,2))
+    self.shadow = zeros((size,size), 'bool')
 
-    self.h = self.one*2
+    self._init_shadow_map()
 
-    self.a = pi*0.3
-    self._set_direction()
-    self.i = 1
+  def _init_shadow_map(self):
+    size = self.size
+    sand = self.sand
+    shadow = self.shadow
+    delta = self.delta
 
-  def get_sand(self):
-    return self.sand
+    for i, height in enumerate(sand):
+      p = 0
+      h = height[p]
+      # if i>10:
+      #   break
+
+      while True:
+        long_shadow = True
+        for d in range(1,delta+1):
+          pd = p+d
+          hd = height[pd]
+          if hd>=h:
+            h = hd
+            long_shadow = False
+            # print(i,p,h,hd,'break')
+            break
+          shadow[i, pd] = True
+
+        p = pd
+        if long_shadow:
+          h = h-1 if h-1>0 else 0
+        if p>=size-delta:
+          break
 
   def get_normalized_sand(self):
     sand = self.sand.astype('float')
@@ -54,58 +78,56 @@ class Dunes(object):
     print(mi, ma)
     return (sand-mi)/(ma-mi)
 
-  def _set_direction(self):
-    self.a += (1.0-2.0*random())*self.angle_stp
-    a = self.a
-    self.dx = array([cos(a), sin(a)], 'float')*self.stp
+  def get_shadow(self):
+    return self.shadow.astype('float')
 
-  def _get_slope(self):
-    size = self.size
-    sand = self.sand
-    xy = self.xy
-    dx = self.dx
-    fij = (((size*(xy+dx))+size)%size).astype('int')
-    bij = (((size*(xy-dx))+size)%size).astype('int')
-    slope = sand[fij[:,0],fij[:,1]] - sand[bij[:,0],bij[:,1]]
-    return slope.astype('float')
+  # def _get_slope(self):
+  #   size = self.size
+  #   sand = self.sand
+  #   xy = self.xy
+  #   dx = self.dx
+  #   fij = (((size*(xy+dx))+size)%size).astype('int')
+  #   bij = (((size*(xy-dx))+size)%size).astype('int')
+  #   slope = sand[fij[:,0],fij[:,1]] - sand[bij[:,0],bij[:,1]]
+  #   return slope.astype('float')
 
-  def _reselsect(self, stopping):
-    size = self.size
-    reselect_num = stopping.sum()
-    new_xy = zeros((reselect_num, 2), 'float')
-
-    # TODO: vectors!!!!
-    count = 0
-    while count<reselect_num:
-      nxy = random((1,2))
-      ij = (size*nxy).astype('int')
-      if self.sand[ij[0,0],ij[0,1]]>0:
-        new_xy[count,:] = nxy
-        count += 1
-
-    nij = (size*new_xy).astype('int')
-    self.xy[stopping,:] = new_xy
-    self.sand[nij[:,0], nij[:,1]] -= 1
+  # def _reselsect(self, stopping):
+  #   size = self.size
+  #   reselect_num = stopping.sum()
+  #   new_xy = zeros((reselect_num, 2), 'float')
+  #
+  #   # TODO: vectors!!!!
+  #   count = 0
+  #   while count<reselect_num:
+  #     nxy = random((1,2))
+  #     ij = (size*nxy).astype('int')
+  #     if self.sand[ij[0,0],ij[0,1]]>0:
+  #       new_xy[count,:] = nxy
+  #       count += 1
+  #
+  #   nij = (size*new_xy).astype('int')
+  #   self.xy[stopping,:] = new_xy
+  #   self.sand[nij[:,0], nij[:,1]] -= 1
 
   def step(self):
-    slope = self._get_slope()
-    # stopping = logical_or(slope>=0,random(slope.shape)>0.95)
-    stopping = slope>0
-    continuing = logical_not(stopping)
-
-    ijs = (self.xy[stopping,:]*self.size).astype('int')
-    self.sand[ijs[:,0],ijs[:,1]] += 1
-
-    self._reselsect(stopping)
-
-    # hh = (1.0+reshape(abs(slope[continuing]),
-    #     (continuing.sum(),1)))*self.dx/10.0
-
-    ijc = (self.xy[continuing,:]*self.size).astype('int')
-    hh = self.sand[ijc[:,0], ijc[:,1]].astype('float')
-    hh = reshape(hh, (len(hh),1))/10.0*self.dx*random((len(hh), 1))
-    # print(norm(hh))
-    self.xy[continuing,:] = (self.xy[continuing,:]+hh)%1.0
-
-    self._set_direction()
+    # slope = self._get_slope()
+    # # stopping = logical_or(slope>=0,random(slope.shape)>0.95)
+    # stopping = slope>0
+    # continuing = logical_not(stopping)
+    #
+    # ijs = (self.xy[stopping,:]*self.size).astype('int')
+    # self.sand[ijs[:,0],ijs[:,1]] += 1
+    #
+    # self._reselsect(stopping)
+    #
+    # # hh = (1.0+reshape(abs(slope[continuing]),
+    # #     (continuing.sum(),1)))*self.dx/10.0
+    #
+    # ijc = (self.xy[continuing,:]*self.size).astype('int')
+    # hh = self.sand[ijc[:,0], ijc[:,1]].astype('float')
+    # hh = reshape(hh, (len(hh),1))/10.0*self.dx*random((len(hh), 1))
+    # # print(norm(hh))
+    # self.xy[continuing,:] = (self.xy[continuing,:]+hh)%1.0
+    #
+    # self._set_direction()
     self.i += 1
