7c5f8e17ec833bb0378cd767c5ef34213d752424
Anders Hoff <inconvergent@gmail.com>
2016-07-16 23:03:41 +0200

    starting pillars

---
+++ main-pillars.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+from numpy import zeros
+
+
+SIZE = 1000
+IMG = './img/gen.png'
+ONE = 1./SIZE
+
+LEAP = 50000
+PROB = 0.3
+
+DELTA = 10
+
+BACK = [1,1,1,1]
+FRONT = [0,0,0,5]
+
+
+
+def main():
+
+  from pillars import Pillars
+  from sand import Sand
+  from fn import Fn
+  from time import time
+
+
+  from modules.helpers import get_initial
+  initial = get_initial(IMG)
+
+  # from modules.helpers import get_initial_rnd
+  # initial = get_initial_rnd()
+  bw = zeros(initial.shape,'float')
+
+  dunes = Pillars(initial, DELTA, PROB)
+
+  sand = Sand(SIZE)
+  sand.set_rgba(FRONT)
+  fn = Fn(prefix='./res/', postfix='.png')
+
+  # try:
+  #   while True:
+  #     t0 = time()
+  #     itt = dunes.steps(LEAP)
+  #     print(itt, time()-t0)
+  #     dunes.get_normalized_sand(bw)
+  #     # bw *= 0.8
+  #     # sand.set_bg_from_bw_array(bw)
+  #     # dunes.get_shadow(shadow)
+  #     # rgb = dstack((bw,bw,shadow))
+  #     # sand.set_bg_from_rgb_array(rgb)
+  #     sand.set_bg_from_bw_array(bw)
+  #     name = fn.name()
+  #     sand.write_to_png(name)
+  #
+  # except KeyboardInterrupt:
+  #   pass
+
+
+if __name__ == '__main__':
+  main()
+
--- setup.py
+++ setup.py
@@ -25,6 +25,11 @@ extensions = [
       sources = ['./src/dunes.pyx'],
       extra_compile_args = _extra,
       include_dirs = [numpy.get_include()]
+      ),
+    Extension('pillars',
+      sources = ['./src/pillars.pyx'],
+      extra_compile_args = _extra,
+      include_dirs = [numpy.get_include()]
       )
     ]
 
