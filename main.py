6cab9f521094df9a68a4b24af6817fc8d3fbb424
Anders Hoff <inconvergent@gmail.com>
2016-07-16 14:05:16 +0200

    converted to cython. missing shuffle on cascade

--- main.py
+++ main.py
@@ -2,15 +2,17 @@
 # -*- coding: utf-8 -*-
 
 from numpy import zeros
-from numpy import ones
 from numpy import dstack
 
 
-SIZE = 512
+SIZE = 1500
 IMG = './img/x512-text.png'
 ONE = 1./SIZE
 
 LEAP = 50000
+PROB = 0.3
+
+DELTA = 10
 
 BACK = [1,1,1,1]
 FRONT = [0,0,0,5]
@@ -19,10 +21,10 @@ FRONT = [0,0,0,5]
 def get_initial(img):
   from numpy.random import random
 
-  # initial = zeros((SIZE,SIZE), 'float')
-  from modules.helpers import get_img_as_rgb_array
-  initial = get_img_as_rgb_array(img)[:,:,0].squeeze()
-  initial *= 20
+  initial = zeros((SIZE,SIZE), 'float')
+  # from modules.helpers import get_img_as_rgb_array
+  # initial = get_img_as_rgb_array(img)[:,:,0].squeeze()
+  # initial *= 20
   initial += random(initial.shape)*15
 
   from scipy.ndimage.filters import gaussian_filter
@@ -37,8 +39,10 @@ def get_initial(img):
   return initial.astype('int')
 
 def save_shadow_map(dunes, sand):
-  bw = dunes.get_normalized_sand()
-  shadow = dunes.get_shadow()
+  bw = zeros((SIZE,SIZE),'float')
+  shadow = zeros((SIZE,SIZE),'float')
+  dunes.get_normalized_sand(bw)
+  dunes.get_shadow(shadow)
   rgb = dstack((zeros(bw.shape,'float'),bw,1.0-shadow))
   sand.set_bg_from_rgb_array(rgb)
   sand.write_to_png('shadow.png')
@@ -46,39 +50,40 @@ def save_shadow_map(dunes, sand):
 
 def main():
 
-  from modules.dunes import Dunes
+  from dunes import Dunes
   from sand import Sand
   from fn import Fn
+  from time import time
 
   initial = get_initial(IMG)
-  print(initial)
-  print()
+  bw = zeros(initial.shape,'float')
+  shadow = zeros(initial.shape,'float')
 
-  dunes = Dunes(initial)
+  dunes = Dunes(initial, DELTA, PROB)
 
   sand = Sand(SIZE)
   sand.set_rgba(FRONT)
   fn = Fn(prefix='./res/', postfix='.png')
 
-  save_shadow_map(dunes, sand)
-
   try:
     while True:
 
-      dunes.steps(steps=LEAP)
-      print(dunes.i)
-      bw = dunes.get_normalized_sand()*0.8
-      # rgb = dstack((bw,bw,bw))
+      t0 = time()
+      itt = dunes.steps(LEAP)
+      print(itt, time()-t0)
+      dunes.get_normalized_sand(bw)
+      # bw *= 0.8
       # sand.set_bg_from_bw_array(bw)
-      shadow = dunes.get_shadow()
-      rgb = dstack((bw,bw,bw+0.1*(1.0-shadow)))
-      sand.set_bg_from_rgb_array(rgb)
+      # dunes.get_shadow(shadow)
+      # rgb = dstack((bw,bw,shadow))
+      # sand.set_bg_from_rgb_array(rgb)
+      sand.set_bg_from_bw_array(bw)
       name = fn.name()
       sand.write_to_png(name)
 
   except KeyboardInterrupt:
     pass
-
+  #
 
 if __name__ == '__main__':
   main()
---
+++ src/dunes.pxd
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+# cython: profile=True
+
+cimport cython
+
+# from libcpp cimport bool
+
+# import numpy as np
+# cimport numpy as np
+
+
+cdef class Dunes:
+
+  cdef int size
+  cdef int delta
+  cdef double prob
+  cdef int i
+  cdef long[:,:] sand
+  cdef long[:,:] shadow
+
+  cdef void _init_shadow_map(self) nogil
+  cdef void _shadow_row(self, const int i) nogil
+  cdef void _random_select(self, int* ij) nogil
+  cdef void _erode(self, const long, const long, const long) nogil
+  cdef int _cascade(self, int i, int j) nogil
+
+  cpdef void get_normalized_sand(self, double[:,:])
+  cpdef void get_shadow(self, double[:,:])
+
+  cpdef int steps(self, int steps)
+
